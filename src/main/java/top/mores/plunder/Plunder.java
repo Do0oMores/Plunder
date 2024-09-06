20769ab16d4f2ecd7de2228b2d2551b26f69f570
Do0oMores <do0omores@outlook.com>
2024-09-07 00:00:03 +0800

    init plugin

---
+++ src/main/java/top/mores/plunder/Plunder.java
@@ -0,0 +1,17 @@
+package top.mores.plunder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plunder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
